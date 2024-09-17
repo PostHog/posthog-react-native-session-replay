6f60181b59940c489aac807e8d69eccb3f8e8ac6
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-17 15:31:01 +0100

    0.0.7

--- android/src/main/java/com/posthogreactnativesessionreplay/PosthogReactNativeSessionReplayModule.kt
+++ android/src/main/java/com/posthogreactnativesessionreplay/PosthogReactNativeSessionReplayModule.kt
@@ -4,8 +4,13 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap;
 
 import com.posthog.PostHog
+import com.posthog.android.PostHogAndroid
+import com.posthog.android.PostHogAndroidConfig
+import com.posthog.internal.PostHogSessionManager
+import java.util.UUID
 
 class PosthogReactNativeSessionReplayModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -22,8 +27,36 @@ class PosthogReactNativeSessionReplayModule(reactContext: ReactApplicationContex
   }
 
   @ReactMethod
+  fun start(sessionId: String, sdkOptions: ReadableMap, sdkReplayConfig: ReadableMap, decideReplayConfig: ReadableMap, promise: Promise) {
+    val uuid = UUID.fromString(sessionId)
+    PostHogSessionManager.setSessionId(uuid)
+
+    val context = this.getReactApplicationContext()
+    val apiKey = sdkOptions.getString("apiKey") ?: ""
+    val host = sdkOptions.getString("host") ?: ""
+
+    val config = PostHogAndroidConfig(apiKey, host)
+    PostHogAndroid.setup(context, config)
+
+    promise.resolve(null)
+  }
+
+  @ReactMethod
+  fun startSession(sessionId: String, promise: Promise) {
+    val uuid = UUID.fromString(sessionId)
+    PostHogSessionManager.setSessionId(uuid)
+    PostHog.startSession()
+    promise.resolve(null)
+  }
+
+  @ReactMethod
   fun isEnabled(promise: Promise) {
     promise.resolve(PostHog.isSessionReplayActive())
+  }
+
+  @ReactMethod
+  fun endSession(promise: Promise) {
+    promise.resolve(null)
   }
 
   companion object {
