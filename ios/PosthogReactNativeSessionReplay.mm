bedf8b3e719d08b2f83440740f000899c6bfafd7
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-18 18:39:57 +0100

    chore: ios plugin

--- ios/PosthogReactNativeSessionReplay.mm
+++ ios/PosthogReactNativeSessionReplay.mm
@@ -6,6 +6,22 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(start:(NSString)sessionId
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(startSession:(NSString)sessionId
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isEnabled:
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(endSession:
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/PosthogReactNativeSessionReplay.swift
+++ ios/PosthogReactNativeSessionReplay.swift
@@ -1,8 +1,32 @@
+import PostHog
+
 @objc(PosthogReactNativeSessionReplay)
 class PosthogReactNativeSessionReplay: NSObject {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
+  }
+  
+  @objc(start:withSdkOptions:withSdkReplayConfig:withDecideReplayConfig:withResolver:withRejecter:)
+  func start(sessionId: String, sdkOptions: [String: Any], sdkReplayConfig: [String: Any], decideReplayConfig: [String: Any], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
+    resolve(nil)
+  }
+  
+  @objc(startSession:withResolver:withRejecter:)
+  func startSession(sessionId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(nil)
+  }
+  
+  @objc(isEnabled:withRejecter:)
+  func isEnabled(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Bool {
+    let isEnabled = false
+    resolve(isEnabled)
+    return isEnabled
+  }
+  
+  @objc(endSession:withRejecter:)
+  func endSession(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(nil)
   }
 }
