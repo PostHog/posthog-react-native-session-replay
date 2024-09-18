00be6d5c9b9840a64395826b885008629c8ce762
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-18 19:11:24 +0100

    fixes

--- ios/PosthogReactNativeSessionReplay.swift
+++ ios/PosthogReactNativeSessionReplay.swift
@@ -10,23 +10,40 @@ class PosthogReactNativeSessionReplay: NSObject {
   
   @objc(start:withSdkOptions:withSdkReplayConfig:withDecideReplayConfig:withResolver:withRejecter:)
   func start(sessionId: String, sdkOptions: [String: Any], sdkReplayConfig: [String: Any], decideReplayConfig: [String: Any], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
+    let apiKey = sdkOptions["apiKey"] as? String ?? ""
+    let host = sdkOptions["host"] as? String ?? PostHogConfig.defaultHost
+    let debug = sdkOptions["debug"] as? Bool ?? false
+
+    PostHogSessionManager.shared.startSession(sessionId)
+
+    let config = PostHogConfig(apiKey: apiKey, host: host)
+    config.sessionReplay = true
+    config.captureApplicationLifecycleEvents = false
+    config.captureScreenViews = false
+    config.debug = debug
+    config.sessionReplayConfig.screenshotMode = true
+    // TODO: all missing config from sdkReplayConfig and decideReplayConfig
+    PostHogSDK.shared.setup(config)
+
     resolve(nil)
   }
   
   @objc(startSession:withResolver:withRejecter:)
   func startSession(sessionId: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    PostHogSessionManager.shared.startSession(sessionId)
+    PostHogSDK.shared.startSession()
     resolve(nil)
   }
   
   @objc(isEnabled:withRejecter:)
-  func isEnabled(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Bool {
-    let isEnabled = false
+  func isEnabled(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    let isEnabled = PostHogSDK.shared.isSessionReplayActive()
     resolve(isEnabled)
-    return isEnabled
   }
   
   @objc(endSession:withRejecter:)
   func endSession(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    PostHogSDK.shared.endSession()
     resolve(nil)
   }
 }
