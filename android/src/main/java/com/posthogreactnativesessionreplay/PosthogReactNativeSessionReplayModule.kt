f3053a2344b55816eebf28f4d1b15f7359752644
Manoel Aranda Neto <marandaneto@gmail.com>
2024-10-25 09:56:06 +0200

    chore: forces the SDK to be initialized on the main thread

--- android/src/main/java/com/posthogreactnativesessionreplay/PosthogReactNativeSessionReplayModule.kt
+++ android/src/main/java/com/posthogreactnativesessionreplay/PosthogReactNativeSessionReplayModule.kt
@@ -4,7 +4,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.UiThreadUtil
 
 import com.posthog.PostHog
 import com.posthog.PostHogConfig
@@ -25,51 +26,60 @@ class PosthogReactNativeSessionReplayModule(reactContext: ReactApplicationContex
 
   @ReactMethod
   fun start(sessionId: String, sdkOptions: ReadableMap, sdkReplayConfig: ReadableMap, decideReplayConfig: ReadableMap, promise: Promise) {
-    val uuid = UUID.fromString(sessionId)
-    PostHogSessionManager.setSessionId(uuid)
+    val initRunnable = Runnable {
+      val uuid = UUID.fromString(sessionId)
+      PostHogSessionManager.setSessionId(uuid)
 
-    val context = this.reactApplicationContext
-    val apiKey = sdkOptions.getString("apiKey") ?: ""
-    val host = sdkOptions.getString("host") ?: PostHogConfig.DEFAULT_HOST
-    val debugValue = sdkOptions.getBoolean("debug")
+      val context = this.reactApplicationContext
+      val apiKey = sdkOptions.getString("apiKey") ?: ""
+      val host = sdkOptions.getString("host") ?: PostHogConfig.DEFAULT_HOST
+      val debugValue = sdkOptions.getBoolean("debug")
 
-    val maskAllTextInputs = sdkReplayConfig.getBoolean("maskAllTextInputs")
-    val maskAllImages = sdkReplayConfig.getBoolean("maskAllImages")
-    val captureLog = sdkReplayConfig.getBoolean("captureLog")
-    val debouncerDelayMs = sdkReplayConfig.getInt("androidDebouncerDelayMs")
+      val maskAllTextInputs = sdkReplayConfig.getBoolean("maskAllTextInputs")
+      val maskAllImages = sdkReplayConfig.getBoolean("maskAllImages")
+      val captureLog = sdkReplayConfig.getBoolean("captureLog")
+      val debouncerDelayMs = sdkReplayConfig.getInt("androidDebouncerDelayMs")
 
-    val endpoint = decideReplayConfig.getString("endpoint")
+      val endpoint = decideReplayConfig.getString("endpoint")
 
-    val distinctId = sdkOptions.getString("distinctId") ?: ""
-    val anonymousId = sdkOptions.getString("anonymousId") ?: ""
-    val theSdkVersion = sdkOptions.getString("sdkVersion")
+      val distinctId = sdkOptions.getString("distinctId") ?: ""
+      val anonymousId = sdkOptions.getString("anonymousId") ?: ""
+      val theSdkVersion = sdkOptions.getString("sdkVersion")
 
-    val config = PostHogAndroidConfig(apiKey, host).apply {
-      debug = debugValue
-      captureDeepLinks = false
-      captureApplicationLifecycleEvents = false
-      captureScreenViews = false
-      sessionReplay = true
-      sessionReplayConfig.screenshot = true
-      sessionReplayConfig.captureLogcat = captureLog
-      sessionReplayConfig.debouncerDelayMs = debouncerDelayMs.toLong()
-      sessionReplayConfig.maskAllImages = maskAllImages
-      sessionReplayConfig.maskAllTextInputs = maskAllTextInputs
+      val config = PostHogAndroidConfig(apiKey, host).apply {
+        debug = debugValue
+        captureDeepLinks = false
+        captureApplicationLifecycleEvents = false
+        captureScreenViews = false
+        sessionReplay = true
+        sessionReplayConfig.screenshot = true
+        sessionReplayConfig.captureLogcat = captureLog
+        sessionReplayConfig.debouncerDelayMs = debouncerDelayMs.toLong()
+        sessionReplayConfig.maskAllImages = maskAllImages
+        sessionReplayConfig.maskAllTextInputs = maskAllTextInputs
 
-      if (!endpoint.isNullOrEmpty()) {
-        snapshotEndpoint = endpoint
+        if (!endpoint.isNullOrEmpty()) {
+          snapshotEndpoint = endpoint
+        }
+
+        if (!theSdkVersion.isNullOrEmpty()) {
+          sdkName = "posthog-react-native"
+          sdkVersion = theSdkVersion
+        }
       }
+      PostHogAndroid.setup(context, config)
 
-      if (!theSdkVersion.isNullOrEmpty()) {
-        sdkName = "posthog-react-native"
-        sdkVersion = theSdkVersion
-      }
+      setIdentify(config.cachePreferences, distinctId, anonymousId)
+
+      promise.resolve(null)
     }
-    PostHogAndroid.setup(context, config)
 
-    setIdentify(config.cachePreferences, distinctId, anonymousId)
-
-    promise.resolve(null)
+    // forces the SDK to be initialized on the main thread
+    if (UiThreadUtil.isOnUiThread()) {
+      initRunnable.run()
+    } else {
+      UiThreadUtil.runOnUiThread(initRunnable)
+    }
   }
 
   @ReactMethod
